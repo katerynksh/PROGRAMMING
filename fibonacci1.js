9e18d66a86b80d94d6e7da6384bfc4e4dfb7590c
Sherepera Kateryna <katerynasherepera@gmail.com>
2025-10-21 15:13:27 +0300

    домашка

---
+++ fibonacci1.js
@@ -0,0 +1,40 @@
+let sum = [0, 1];
+
+console.log("fibonacci without recursion")
+
+function fibonacci(guantity) {
+    for (let i = 0; i < guantity; i++) {        
+        let next = sum[sum.length - 1] + sum[sum.length - 2];
+        sum.push(next);  
+        if (sum.length > guantity) {
+            sum.pop();
+        }
+    }
+    console.log("fibonacci:", sum);
+}
+
+fibonacci("3", 3); 
+fibonacci("5", 5); 
+fibonacci("7", 7); 
+
+
+console.log("Recursion")
+
+function fibonacciRecursion(guantity, m = 2, n =3, sumrec = []) {
+    //console.log(`guantity: ${guantity}, m: ${m}, n: ${n}`);    
+    //console.log("fibonacciRecursion:", sumrec);
+    if (guantity <= 0) {
+        return sumrec;
+    }
+    sumrec.push(m);    
+    return fibonacciRecursion(guantity - 1, n, m + n, sumrec);
+}
+
+const a = fibonacciRecursion(3);
+console.log("fibonacciRecursion 3:", a);
+
+const b = fibonacciRecursion(5);
+console.log("fibonacciRecursion 5:", b);
+
+const c = fibonacciRecursion(7);
+console.log("fibonacciRecursion 7:", c);
---
+++ numbersArray1.js
@@ -0,0 +1,23 @@
+let numArr = []
+for(let i = 0 ; i < 10; i++) {
+    numArr[i] = Math.round(Math.random() * 100 - 50)
+}
+console.log(numArr)
+
+let i = 1
+
+
+function sorting(vidro) {
+
+for(let j = 0; j < numArr.length - 1; j++) {
+    for(let i = j + 1; i < numArr.length; i++) {
+        if(numArr[j] >= numArr[i]) {
+            let temp = numArr[j]
+            numArr[j] = numArr[i]
+            numArr[i] = temp
+        }
+    }
+}
+}
+return i
+console.log(numArr)
