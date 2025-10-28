021d0bad93b98a7f54147d88f8069959da2563f6
Sherepera Kateryna <katerynasherepera@gmail.com>
2025-10-28 11:17:31 +0200

    завдання 28.10

---
+++ negativeNumber.js
@@ -0,0 +1,21 @@
+const toFilter = [3, 5, 6, -1, -7, 12, `lalala`, null, undefined, -0.5, -9, `Tanya`, `42`, ]
+
+function negativeFiltering(arr =[]) {
+
+    const negativeNumber = []
+
+    if(!Array.isArray(arr)){
+    return `Це не масив`
+    }
+    else console.log("Оригінальний масив:",arr)
+
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            negativeNumber.push(arr[i])
+        }
+    
+    }
+    return negativeNumber
+}
+const a = negativeFiltering(toFilter)
+console.log(`від'ємні числа:`, a)
