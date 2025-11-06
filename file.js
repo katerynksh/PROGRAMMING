17e41cadbc35f3abe0e198fbdd8b6d805ee366a4
Sherepera Kateryna <katerynasherepera@gmail.com>
2025-11-06 18:36:38 +0200

    ‼️домашка на 07.10.25

---
+++ file.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const data = fs.readFileSync(
+    'book.txt',
+    {
+        encoding: 'utf8',
+        flag: 'r'
+    }
+);
+console.log(data.replaceAll(" ", " ✨Катятятя✨ "));
