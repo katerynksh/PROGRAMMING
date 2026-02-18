2009b9f8fe225057df35534a5bd09281afa23407
katerynksh <shereperakatia@gmail.com>
2026-02-18 22:58:51 +0200

    not done yet

--- 2 семестр/lessons/counter_stream.js
+++ 2 семестр/lessons/counter_stream.js
@@ -30,44 +30,74 @@ console.table(sortedEntries);
 
 
 import { createReadStream } from 'node:fs';
-const readFile = createReadStream(
-    'book.txt',
+import { exit } from 'node:process';
+import path from 'path'
+
+const parsed = path.parse(process.argv[1]);
+
+if (!process.argv[2]) {
+  console.log(`Роззуй очі!: node ${parsed.base} <input file name>`)
+  exit(0);
+}
+
+const readFile = createReadStream(process.argv[2],
     {
         encoding: 'utf8',
-        flag: 'r'
+        flag: 'r',
+        highWaterMark:100
     }
 );
 
 let allSymbols = 0;
 const counters = {};
-const words = {};
+
+let allWords = 0
+const wordCount = {}
+
 readFile.on('data', (chunk) => {
     allSymbols += chunk.length;
     // console.log("Chunk => ", chunk);
     console.log("All symbols => ", allSymbols);
+
     for (let i = 0; i < chunk.length; i++) {
         if (counters[chunk[i]] === undefined) {
             counters[chunk[i]] = 0
         }
         counters[chunk[i]] += 1;
-    // }
-    // for (let i = 0; i < chunk.length; i++) {
-    //     if (chunk[i] === ' ' || chunk[i] === '\n') {
-    //         continue;
-    //     }
-    //     if (words[chunk[i]] === undefined) {
-    //         words[chunk[i]] = 0
-    //     }
-    //     words[chunk[i]] += 1;
-    // }
+    }
+ 
+    // \p{L} - будь-яка буква (Unicode)
+    // \p{N} - будь-яка цифра
+    // [^\p{L}\p{N}]+ - шукаємо групи символів, що НЕ є буквами чи цифрами
 
-    const entries = Object.entries(counters);
-    const sortedEntries = entries.sort((a, b) => b[1] - a[1]);
-    const wordEntries = Object.entries(words);
-    
-console.log("Numbers of each symbol:");
-console.table(sortedEntries);
-console.log("Numbers of each word:");
-console.table(wordEntries);
+    const words = chunk
+    .toLowerCase()
+    .split(/[^\p{L}\p{N}]+/u)
+    .filter(word => word.length > 0);
+
+        allWords += words.length;
+
+    for (const word of words) {
+    //for (let word = 0; word < chunk.length; word++) {
+        if (wordCount[word] === undefined){
+            wordCount[word] = 0
+    }
+    wordCount[chunk[word]] += 1;
+    }
+
+
+
 });
 
+readFile.on('end', () => {
+// console.table(Object.entries(counters).sort((a, b) => b[1] - a[1]))
+
+    const sortedSymbols = Object.entries(counters).sort((a, b) => b[1] - a[1]);
+    const sortedWords = Object.entries(wordCount).sort((a, b) => b[1] - a[1]);
+        
+    console.log("Numbers of each symbol:");
+    console.table(sortedSymbols);
+
+    console.log("Number of all words:", + allWords)
+    console.table("Number of every word:" + sortedWords)
+});
